10cf605dce99f13333194e58cbcf0334f205aa22
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-11-27 12:48:18 -0300

    Finalização da versão inicial do Jogo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import './App.css';
 
 function Square({ value, onSquareClick }) {
@@ -18,7 +19,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -28,8 +29,18 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
 
   return <>
+    <div className='status'>{status}</div>
     <div className='board-row'>
       <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
       <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -46,4 +57,27 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
   </>
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+  for (const element of lines) {
+    const [a, b, c] = element;
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
